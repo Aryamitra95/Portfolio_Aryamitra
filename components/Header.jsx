d08a32f12f22b061c57aedd660a1cfe1c6825689
Aryamitra95 <aryamitra.pradhan@gmail.com>
2025-06-30 19:05:01 +0530

    first commit

---
+++ components/Header.jsx
@@ -0,0 +1,34 @@
+import Link from 'next/link'
+import React from 'react'
+import Nav from './Nav'
+import { Button } from './ui/button'
+import MobileNav from './MobileNav'
+
+const Header = () => {
+  return (
+    <header className='py-8 xl:py-12 text-white'>
+        <div className='container mx-auto flex justify-between items-center'>
+            <Link href="/">
+            <h1 className='text-4xl font-semibold'>
+                Aryamitra <span className='text-accent'>.</span>
+            </h1>
+            </Link>
+
+            <div className='hidden xl:flex items-cneter gap-8'>
+                <Nav/>
+                <Link href="/contact">
+                    <Button>
+                        Hire Me
+                    </Button>
+                </Link>
+            </div>
+
+            <div className='xl:hidden'>
+                <MobileNav/>
+            </div> 
+        </div>
+    </header>
+  )
+}
+
+export default Header
