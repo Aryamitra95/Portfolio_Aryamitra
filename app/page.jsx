d6974e3739f0fef52a98e37b411d350910ffa2a9
Aryamitra95 <aryamitra.pradhan@gmail.com>
2025-07-01 09:18:16 +0530

    eslint version managed

--- app/page.jsx
+++ app/page.jsx
@@ -8,7 +8,7 @@ export default function Home() {
   return (
     <section className="h-full">
       <div className="container mx-auto h-full">
-        <div className="flex flex-col xl-flex-row items-center justify-between xl:pt-8 xl:pb-24">
+        <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
           <div className="text-center xl:text-left order-2 xl:order-none">
             <span className="text-xl">Software Developer</span>
             <h1 className="h1 mb-6">Hello I'm <br/> <span className="text-accent">Aryamitra Pradhan</span></h1>
